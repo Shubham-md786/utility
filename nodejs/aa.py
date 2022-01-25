32ab11f944c36760a63b0730395201b1e311423b
Shubham Dhameliya <shubhamdhameliyaweb@gmail.com>
2022-01-25 20:40:38 +0530

    modified

---
+++ nodejs/aa.py
@@ -0,0 +1,1 @@
+function base64_encode(file) {
--- nodejs/enc_dec/bin/decFile.js
+++ nodejs/enc_dec/bin/decFile.js
@@ -1,22 +1,30 @@
 #! /usr/bin/env node
 
+//core module
 const fs = require('fs');
+const path = require('path');
+//npm module
 const chalk = require('chalk');
-const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-readline.question('Enter file path name => ', name => {
+const yargs = require("yargs");
+
+const inputFile = yargs.argv._;
+
+
+if(!inputFile) return console.log(chalk.red.bold('No file provided for decryption'))
+
+inputFile.forEach(name =>{
 	try {
+
 		let base64File = fs.readFileSync(name, 'utf8')
 		const arr = base64File.toString().replace(/\r\n/g, '\n').split('\n');
 		let decode = Buffer.from(arr[1], 'base64')
 		fs.writeFile(arr[0], decode, function(err) {
 			console.log(chalk.green.bold(`\nFile created suceesfully => ${arr[0]}\n`));
 		})
-		readline.close();
+
 	} catch(err) {
+
 		console.log(chalk.red.bold(`\nFile not found\n`));
-		readline.close();
+
 	}
 })
